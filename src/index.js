505d5a998658cac159516b1962a17a3fa178ef19
Michael Duberstein <dubermic@flutterint.com>
2022-07-08 21:57:28 -0400

    Component: After Clip 50 Refactoring from Functional to Class Components

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <div>
-      <div>Seasons</div>
-      <SeasonDisplay />
-    </div>
-  );
-};
+class App extends React.Component {
+  render () {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return (
+      <div>
+        <div>Latitude: </div>
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+ 
