a1770868ef951d12283c63c91f6dd14ebe295c85
Michael Duberstein <dubermic@flutterint.com>
2022-07-09 13:45:39 -0400

    Seasons: After Clip 65 Showing Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -14,11 +14,14 @@ const SeasonDisplay = (props) => {
   console.log(`Props.lat = ${props.lat}`);
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
       {/* // Alternative 2, showing use of ternary expression inside curly braces of JSX: 
       // {season === "winter" ? "Burr, it is chilly" : "Let's hit the beach"} */}
+      <i className={`${icon} icon`} />
     </div>
   );
 };
