b9ce4a1c1bd0cb91b51b64a8d365194d46ddd12c
Michael Duberstein <dubermic@flutterint.com>
2022-07-09 13:56:13 -0400

    Seasons: After Clip 66 Extracting Options to Config Objects

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'   
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     // nothern hemisphere : sothern hemispher
@@ -13,15 +24,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   console.log(`Props.lat = ${props.lat}`);
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  // ES2015 destructuring syntax
+  const {text, iconName} = seasonConfig[season];
+
   return (
     <div>
-      <i className={`${icon} icon`} />
-      <h1>{text}</h1>
-      {/* // Alternative 2, showing use of ternary expression inside curly braces of JSX: 
-      // {season === "winter" ? "Burr, it is chilly" : "Let's hit the beach"} */}
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1> 
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
