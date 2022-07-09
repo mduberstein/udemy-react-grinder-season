7d8136337fe582234099767395e587245babb4f7
Michael Duberstein <dubermic@flutterint.com>
2022-07-09 15:34:05 -0400

    Seasons: After Clip 69 Specifying Default Props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Spinner = () => {
+const Spinner = (props) => {
   return (
     <div class="ui active dimmer">
-      <div class="ui big text loader">Loading...</div>
+      {/* simplistic way to define default in case no props are passed by parent */}
+      {/* <div class="ui big text loader">{props.message || "Loading ..."}</div> */}
+      <div class="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+Spinner.defaultProps = {
+  message: 'Loading...'
 };
 
 export default Spinner;
