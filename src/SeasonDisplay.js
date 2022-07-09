8e29725c33d4a4c754cd24aeccbb258588c3346f
Michael Duberstein <dubermic@flutterint.com>
2022-07-09 13:08:37 -0400

    Seasons: After Clip 62 Determining Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    // nothern hemisphere : sothern hemispher
+    return lat > 0 ? 'summer': 'winter';
+  } else {
+    // nothern hemisphere : sothern hemispher
+    return lat > 0 ? 'winter': 'winter';
+  }
+  
+}
+
 const SeasonDisplay = (props) => {
   console.log(`Props.lat = ${props.lat}`);
+  const season = getSeason (props.lat, new Date().getMonth());
+  console.log(season);
   return (
     <div>Season Display</div>
   );
