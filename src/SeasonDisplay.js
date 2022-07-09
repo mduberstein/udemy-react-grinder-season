f9a71e77f04bcf4f49cfea0337527eca59bfdcdd
Michael Duberstein <dubermic@flutterint.com>
2022-07-09 13:18:12 -0400

    Seasons: After Clip 63 Ternary expressions in JSX

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const getSeason = (lat, month) => {
-  if(month > 2 && month < 9) {
+  if (month > 2 && month < 9) {
     // nothern hemisphere : sothern hemispher
-    return lat > 0 ? 'summer': 'winter';
+    return lat > 0 ? "summer" : "winter";
   } else {
     // nothern hemisphere : sothern hemispher
-    return lat > 0 ? 'winter': 'winter';
+    return lat > 0 ? "winter" : "winter";
   }
-  
-}
+};
 
 const SeasonDisplay = (props) => {
   console.log(`Props.lat = ${props.lat}`);
-  const season = getSeason (props.lat, new Date().getMonth());
-  console.log(season);
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
   return (
-    <div>Season Display</div>
+    <div>
+      <h1>{text}</h1>
+      {/* // Alternative 2, showing use of ternary expression inside curly braces of JSX: 
+      // {season === "winter" ? "Burr, it is chilly" : "Let's hit the beach"} */}
+    </div>
   );
 };
 
