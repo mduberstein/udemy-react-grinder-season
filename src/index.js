82fe3b688ef614c02070f2dc64e70bd0efba6a40
Michael Duberstein <dubermic@flutterint.com>
2022-07-08 22:16:29 -0400

    Component: After Clip 52 Initializing State Through Constructors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  constructor (props) {
+    // call to parent constructor
+    super(props);
+    this.state = { lat: null };
+  }
+
+  // render is required by React
   render () {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
